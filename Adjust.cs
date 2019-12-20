d881c2beddd984bc1070d1358b6110c17a55d1b1
Mycrafte <keeptpa@qq.com>
2019-12-20 17:07:55 +0800

    lacking for tags, need help

---
+++ Adjust.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Harmony;
+using UnityEngine;
+
+namespace Patches
+{ 
+    [HarmonyPatch(typeof(ExteriorWallConfig), "CreateBuildingDef")]
+    public static class ExteriorWallAdjust
+    {
+
+         public static void Postfix(ref BuildingDef __result)
+         {
+            __result.ConstructionTime = 3f;
+            __result.BuildLocationRule = BuildLocationRule.Anywhere;
+
+            __result.ReplacementTags = new List<Tag>
+            {
+                 //Who can tell me which TAG does the Drywall&Tempshift wall use
+     
+            };
+            __result.EquivalentReplacementLayers = new List<ObjectLayer>
+            {
+                  ObjectLayer.ReplacementTile
+            };
+        }
+
+
+    }
+
+
+    [HarmonyPatch(typeof(ThermalBlockConfig), "CreateBuildingDef")]
+    public static class TempShiftAdjust
+    { 
+        public static void Postfix(ref BuildingDef __result)
+        { 
+              __result.ConstructionTime = 3f;
+        }
+    }
+}
